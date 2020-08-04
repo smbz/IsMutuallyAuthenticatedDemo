23be27ff76bfbc86e39857175e594ebb154de615
Andrew Ryrie <smb314159@gmail.com>
2020-08-04 15:04:12 +0100

    Initial commit

---
+++ IsMutuallyAuthenticatedDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Security;
+using System.Net.Sockets;
+
+namespace IsMutuallyAuthenticatedDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "www.microsoft.com";
+            TcpClient client = new TcpClient(host, 443);
+            SslStream stream = new SslStream(client.GetStream());
+            stream.AuthenticateAsClient(host);
+            if (stream.IsMutuallyAuthenticated)
+            {
+                Console.WriteLine("Both client and server are authenticated");
+            }
+            else
+            {
+                Console.WriteLine("Not mutually authenticated");
+            }
+        }
+    }
+}
